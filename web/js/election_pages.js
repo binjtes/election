66d7f5be2d0ea25dad0a282e5949a7db11817e84
binjtes <bintjes@gmail.com>
2015-02-03 06:35:21 +0100

    add party form
    using data-prototype . to finish

--- web/js/election_pages.js
+++ web/js/election_pages.js
@@ -1,5 +1,24 @@
-$( '#createbutton' ).click(function() {
+$( '#createbutton' ).click(function(event) {
+	event.preventDefault();
 	$('#formhidden').show("slow");
 	$( '#createbutton' ).hide();
 });	
 
+
+// country edit : add party button
+$( '#createparty' ).click(function(event) {
+	 event.preventDefault();
+	 var formcreateparty = $( 'div#bepselectionbundle_country_parties');
+	 if(formcreateparty != null){
+		 // TODO : ne marche pas.
+		 var index = formcreateparty.find(':index').length ;
+		 var prototype = $(formcreateparty.attr('data-prototype').replace(/__name__label__/g, 'party n°' + (index+1)).replace(/__name__/g, index));
+		 // add form
+		 formcreateparty.append(prototype) ;
+	 
+	 }
+	 
+	 
+	 
+
+});
