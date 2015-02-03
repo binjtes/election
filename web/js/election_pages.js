0b93424494d3c35d46a413d6ef63f724f394dd83
binjtes <bintjes@gmail.com>
2015-02-03 19:30:48 +0100

    party form
    index working + hide country choice

--- web/js/election_pages.js
+++ web/js/election_pages.js
@@ -10,15 +10,13 @@ $( '#createparty' ).click(function(event) {
 	 event.preventDefault();
 	 var formcreateparty = $( 'div#bepselectionbundle_country_parties');
 	 if(formcreateparty != null){
-		 // TODO : ne marche pas.
-		 var index = formcreateparty.find(':index').length ;
+		 
+		 var index = formcreateparty.find("div[class='form-group']:first-child").length ;
 		 var prototype = $(formcreateparty.attr('data-prototype').replace(/__name__label__/g, 'party n°' + (index+1)).replace(/__name__/g, index));
-		 // add form
-		 formcreateparty.append(prototype) ;
-	 
+		 
+		 formcreateparty.append(prototype) ;	
+		 // the country will be hidden and so its label : we do not need it , we're on a selected country 
+		 formcreateparty.find("#bepselectionbundle_country_parties_"+index+"_country").parent().hide();
 	 }
-	 
-	 
-	 
 
 });
